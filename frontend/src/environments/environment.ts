8a8d03c316fdddd672f23dfeea222066156c79b0
Khalil Al-Rashid <rashedki@hotmail.com>
2020-07-08 17:19:21 +0300

    general update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'rashedki.us', // the auth0 domain prefix
+    url: 'rashedki', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'StVOI7GLpaTS2xvT685uv1UGew2Ydj1b', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
